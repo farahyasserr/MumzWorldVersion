e4cb4485fccd1c5d48f8a15059c836122f3b9212
farahdawoud <farah@qashio.com>
2024-06-30 21:35:58 +0400

    feat(shared): Dividor shared component

---
+++ src/shared/Dividor/Dividor.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import colors from "../../theme/colors";
+
+export const styles = StyleSheet.create({
+    dividor: {
+        borderColor: colors.grey,
+        borderWidth: 0.4,
+        width: '100%',
+        alignSelf: 'center',
+        marginVertical: 10,
+        opacity: 0.2
+    }
+})
---
+++ src/shared/Dividor/Dividor.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import { styles } from './Dividor.styles'
+
+const Dividor = () => {
+    return (
+        <View style={styles.dividor} />
+    )
+}
+
+export default Dividor
