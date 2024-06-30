5c4522f9def80f31725133fa13f12383cc54962a
farahdawoud <farah@qashio.com>
2024-06-30 21:50:05 +0400

    feat(shared): Shared components index

---
+++ src/shared/index.ts
@@ -0,0 +1,12 @@
+export { default as ToggleLanguageButton } from './ToggleLanguageButton';
+export { default as ImageCarousel } from './ImageCarousel';
+export { default as TitleValueItem } from './TitleValueItem';
+export { default as SearchBar } from './SearchBar';
+export { default as ProductItem } from './ProductItem';
+export { default as AddToWishlistButton } from './AddToWishlistButton';
+export { default as Dividor } from './Dividor';
+export { default as Price } from './Price';
+export { default as EmptySearchView } from './EmptySearchView';
+
+
+
