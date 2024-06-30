59ad967b66b5c5c734fe88f4c67dcd7ee014ad4c
farahdawoud <farah@qashio.com>
2024-06-30 21:47:43 +0400

    feat(theme): Colors object created

---
+++ src/theme/colors.ts
@@ -0,0 +1,13 @@
+const colors = {
+    white: '#FFFFFF',
+    black: '#000000',
+    grey: '#71717A',
+    primary: '#C30045',
+    orange: '#F67416',
+    blue: '#0070BE',
+    lightGrey: '#E4E4E7',
+    lightestGrey: '#F4F4F5',
+    test: '#fef1f1'
+};
+
+export default colors;
