efa2b5d8c173190215711131ffe70ecce3fb6003
farahdawoud <farah@qashio.com>
2024-06-30 21:46:33 +0400

    feat(carousel): Dummy data for image gallery

---
+++ src/constants/dummyCarouselData.ts
@@ -0,0 +1,22 @@
+export const carouselImages = [
+    {
+        id: 1,
+        uri: 'https://www.mumzworld.com/media/catalog/product/cache/8bf0fdee44d330ce9e3c910273b66bb2/g/f/gf-he0605-huanger-baby-toys-piano-fitness-rack-blue-1600254265.jpg',
+    },
+    {
+        id: 2,
+        uri: 'https://www.mumzworld.com/media/catalog/product/cache/8bf0fdee44d330ce9e3c910273b66bb2/g/f/gf-he0605-huanger-baby-toys-piano-fitness-rack-blue-1600254265.jpg',
+    },
+    {
+        id: 3,
+        uri: 'https://www.mumzworld.com/media/catalog/product/cache/8bf0fdee44d330ce9e3c910273b66bb2/g/f/gf-he0605-huanger-baby-toys-piano-fitness-rack-blue-1600254265.jpg',
+    },
+    {
+        id: 4,
+        uri: 'https://www.mumzworld.com/media/catalog/product/cache/8bf0fdee44d330ce9e3c910273b66bb2/g/f/gf-he0605-huanger-baby-toys-piano-fitness-rack-blue-1600254265.jpg',
+    },
+    {
+        id: 5,
+        uri: 'https://www.mumzworld.com/media/catalog/product/cache/8bf0fdee44d330ce9e3c910273b66bb2/g/f/gf-he0605-huanger-baby-toys-piano-fitness-rack-blue-1600254265.jpg',
+    }
+]
